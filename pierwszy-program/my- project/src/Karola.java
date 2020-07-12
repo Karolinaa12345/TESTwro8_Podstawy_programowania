d91901b8287d20e0cf8a81df046a214c16ed0f26
Karolina <karola.kosciuczyk@gmail.com>
2020-07-12 14:40:17 +0200

    Mój trzeci program

---
+++ pierwszy-program/my- project/src/Karola.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Karola {
+    public static void main(String... args) {
+        System.out.println("Witaj w programi");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj ciąg znaków");
+        String string = scanner.next ();
+        System.out.println("Podaj pozycję znaku");
+        int index = scanner.nextInt();
+
+
+
+        System.out.println("Ciąg" + string + "zawiera"
+                + string.charAt(index) + "na pozycji"
+                + index);
+    }
+}
+
+
+
+
