6175f171525307b47a0ef6a27941c57203c4ead8
Karolina <karola.kosciuczyk@gmail.com>
2020-07-12 12:47:34 +0200

    Mój pierwszy program

---
+++ pierwszy-program/my- project/src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+   public static void main(String[] args){
+       System.out.println("Witaj w programie!");
+       String name = "Karolina";
+       String surname = "Kosciuczyk";
+       System.out.println(name+" "+surname);
+       System.out.println("Koniec");
+    }
+
+}
