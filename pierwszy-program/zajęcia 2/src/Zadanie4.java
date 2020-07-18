4599054a04da4606cdab258b636c05578cc8f922
Karolina <karola.kosciuczyk@gmail.com>
2020-07-18 12:33:55 +0200

    zajęcia z 18-07-20 , zadanieCzwarte

--- pierwszy-program/zajęcia 2/src/Zadanie4.java
+++ pierwszy-program/zajęcia 2/src/Zadanie4.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Zadanie4 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Witaj!""Proszę podaj wiek");
+        System.out.println("Witaj!Proszę podaj wiek");
         int wiek = scanner.nextInt();
         if (wiek >= 18)
             System.out.println("Nie jestem zaproszony na impreze");
